2f2ceb540cc867ecd0f65aa83137cefad6a28e9b
Nguyễn Bảo An <khiconst4@gmail.com>
2025-10-18 21:19:47 +0700

    tạo các file servlet, service , dao dùng để gom tổng các dữ liệu
    tổng donate, tổng sự kiện, tổng số tài khoản, tổng số người dùng

---
+++ src/java/dao/DonationDAO.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import utils.DBContext;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DonationDAO {
+
+    private java.sql.Connection conn;
+
+    public DonationDAO() {
+        try {
+            DBContext db = new DBContext();
+            this.conn = db.getConnection(); // lấy connection từ DBContext
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // truy vấn tổng tiền donation trong database
+    public double getTotalDonationAmount() {
+        double total = 0;
+        String sql = "SELECT SUM(amount) FROM donation";
+        try (PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+            if (rs.next()) {
+                total = rs.getDouble(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return total;
+    }
+
+}
