6d14f18917891829987df6e3dcaea51780eb2282
Nguyễn Bảo An <khiconst4@gmail.com>
2025-10-30 13:36:16 +0700

    ....

--- src/java/dao/UserDAO.java
+++ src/java/dao/UserDAO.java
@@ -87,29 +87,4 @@ public class UserDAO {
         }
         return false;
     }
-
-//    // 4. Tạo user profile sau khi tạo account
-//    public boolean insertUser(int accountId, String fullName, String email, String phone, 
-//                              String gender, String dob, String address, String avatar, String jobTitle, String bio) {
-//        String sql = "INSERT INTO Users (account_id, full_name, dob, gender, phone, email, address, avatar, job_title, bio) " +
-//                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-//        try (PreparedStatement ps = conn.prepareStatement(sql)) {
-//            ps.setInt(1, accountId);
-//            ps.setString(2, fullName);
-//            ps.setDate(3, dob != null && !dob.isEmpty() ? Date.valueOf(dob) : null);
-//            ps.setString(4, gender != null && !gender.isEmpty() ? gender : null);
-//            ps.setString(5, phone != null && !phone.isEmpty() ? phone : null);
-//            ps.setString(6, email);
-//            ps.setString(7, address != null && !address.isEmpty() ? address : null);
-//            ps.setString(8, avatar != null && !avatar.isEmpty() ? avatar : null);
-//            ps.setString(9, jobTitle != null && !jobTitle.isEmpty() ? jobTitle : null);
-//            ps.setString(10, bio != null && !bio.isEmpty() ? bio : null);
-//            
-//            int rows = ps.executeUpdate();
-//            return rows > 0;
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return false;
-//    }
 }
